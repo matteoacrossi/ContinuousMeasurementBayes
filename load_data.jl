0e97d9b75cc73f387000387cfbeaa38ba1cd32d1
Matteo Rossi <teo.red90@gmail.com>
2019-02-22 16:19:34 +0200

    Updated load_data script

--- load_data.jl
+++ load_data.jl
@@ -1,63 +1,38 @@
 using HDF5
-using Statistics
 
-FILENAME = "093_exp=1_prepared.h5"
+"""
+Load the data corresponding to the final z measurements.
 
-file = h5open(FILENAME, "r")
+Returns a named tuple containing the three currents (rescaled)
+and the output of the strong measurement.
+"""
+function load_z_data(file)
+    OutZ = read(file["z"])
+    CHUNK_SIZE = 10000
+    chunk_n = Int(floor(length(OutZ) / CHUNK_SIZE))
+    indices = vcat([1 + (-1 + 3 * i ) * CHUNK_SIZE : 3* i * CHUNK_SIZE for i in 1:chunk_n]...)
+    print(size(OutZ))
+    dyHet1 = read(file["u"])
+    dyHet1 = rescale_experimental_data.(dyHet1[:,indices])
+    print(size(dyHet1))
+    dyHet2 = read(file["v"])
+    dyHet2 = rescale_experimental_data.(dyHet2[:,indices])
+    print(size(dyHet2))
+    dyDep =  read(file["w"])
+    dyDep = rescale_experimental_data.(dyDep[:,indices])
+    print(size(dyDep))            
+    return (dyHet1=dyHet1, dyHet2=dyHet2, dyDep=dyDep, OutZ=OutZ)
+end
 
-u = read(file["u"])
-v = read(file["v"])
-w = read(file["w"])
+function sample_data(Ntraj, dataTuple)
+    idx = sample(1:length(dataTuple.OutZ), Ntraj; replace=false, ordered=true)
+    dyHet1 = dataTuple.dyHet1[:,idx]
+    dyHet2 = dataTuple.dyHet2[:,idx]
+    dyDep  = dataTuple.dyDep[:,idx]
+    OutZ = dataTuple.OutZ[idx]
+    return (dyHet1=dyHet1, dyHet2=dyHet2, dyDep=dyDep, OutZ=OutZ)
+end
 
-rescaling_coefficient = -0.15 / mean(w, dims=2)[1] 
-
-dyHet1 = rescaling_coefficient .* u
-dyHet2 = rescaling_coefficient .* v
-dyDep = rescaling_coefficient .* w
-
-# Parameters
-Ntrajectories = 20
-Tfinal = 20. # Final time
-dt = 0.1 # duration of infinitesimal time
-Gamma1 = 1. / 15   # Gamma fluoresence
-GammaD = 1. / 0.3   # Gamma dephasing controllable
-GammaPhi = 1. / 17.9  # Gamma dephasing not controllable
-etavalF = 0.14 # efficiency fluoresence heterodyne
-etavalD = 0.34 # efficiency dephasing homodyne
-omegaTrue = 2 * pi / 5. # True value of omega
-omegaMin= 0. # minimum value of omega
-omegaMax= 3. # maximum value of omega
-Nomega = 200 # Resolution in omega for the Bayesian estimation
-Ntime = 197
-
-t = dt * 1:Ntime
-include("fluo_cont_meas_sim.jl")
-include("likelihood_all.jl")
-
-@time (t, omegay, AvgZcond, probBayes, probBayesTraj, omegaEst, omegaMaxLik, sigmaBayes)= LikelihooodTrajNew(Ntrajectories, Ntime;
-    Tfinal = t[end], # Final time
-    Gamma1 = Gamma1 ,   # Gamma fluoresence
-    GammaD = GammaD,    # Gamma dephasing controllable
-    GammaPhi = GammaPhi,  # Gamma dephasing not controllable
-    etavalF = etavalF, #efficiency fluoresence heterodyne
-    etavalD = etavalD, #efficiency dephasing homodyne
-    Nomega = Nomega, # number of values of omega
-    omegaTrue = omegaTrue, #true value of omega
-    omegaMin = omegaMin,
-    omegaMax = omegaMax,
-    dyHet1 = dyHet1, #output current 1 heterodyne (simgax)
-    dyHet2 = dyHet2, #output current 2 heterodyne (simgay)
-    dyDep = dyDep)#,  #output current 3 homodyne (sigmaz)
-    #OutS = OutStrong ); # output strong final measurement
-
-    using PyPlot
-
-figure()
-plot(t, omegaEst, label="Bayes")
-plot(t, omegaEst + sigmaBayes, "C0--")
-plot(t, omegaEst - sigmaBayes, "C0--")
-plot(t, omegaMaxLik, label="Max-Lik")
-plot(t, fill(omegaTrue, size(t)), label="True")
-legend()
-#savefig("bayes.pdf")
-#close()
+function rescale_experimental_data(x, factor=10^(-3/2))
+    x .* factor
+end
