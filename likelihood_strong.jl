2e1b749519f0ead52471d4f2e20479efd02ee926
Matteo Rossi <teo.red90@gmail.com>
2019-03-08 10:07:30 +0200

    fixed correction factor

--- likelihood_strong.jl
+++ likelihood_strong.jl
@@ -40,19 +40,16 @@ dimJ = 2 # Dimension of the corresponding Hilbert space
 unconditional_timesteps = 3
 
 # We take into account the internal timesteps
-Ntime *= internalsteps
+# Ntime *= internalsteps
 dt = Tfinal / Ntime
-unconditional_timesteps *= internalsteps
+# unconditional_timesteps *= internalsteps
 
 if internalsteps > 1
     # We devide the currents by the number of internal timesteps and
     # repeat each element for that number
-    dyHet1 ./= internalsteps
-    dyHet1 = interpolate_current(dyHet1, internalsteps)
-    dyHet2 ./= internalsteps
-    dyHet2 = interpolate_current(dyHet2, internalsteps)
-    dyDep ./= internalsteps
-    dyDep = interpolate_current(dyDep, internalsteps)
+    dyHet1 = interpolate_current(dyHet1 / internalsteps, internalsteps)
+    dyHet2 = interpolate_current(dyHet2 / internalsteps, internalsteps)
+    dyDep = interpolate_current(dyDep / internalsteps, internalsteps)
 end
 Ntraj = size(dyHet1,2)
     
@@ -115,61 +112,66 @@ sigmaStrong = Array{Float64}(undef, Ntraj);
 omegaMaxLikStrong = Array{Float64}(undef, Ntraj);
                                 
 AvgZcond = Array{Float64}(undef, Ntraj,Ntime)
- 
+
+idt = dt / internalsteps
+
 H = [(o/2.) * sy for o in omegay]; # Hamiltonian of the qubit
 
-M0 = I - ((cF'*cF)/2 + (cD'*cD)/2 + (cPhi'*cPhi)/2) * dt
+M0 = I - ((cF'*cF)/2 + (cD'*cD)/2 + (cPhi'*cPhi)/2) * idt
 rho0 = cat([RhoIn for i=1:Nomega+1]..., dims=3)
 
 for ktraj = 1:Ntraj
     copy!(rho, rho0)
     for jt=1:Ntime
-
-        if jt <= unconditional_timesteps
-            M1 = M0;
-        else
-            M1 = M0 + sqrt(etavalF/2) * cF * (dyHet1[end - Ntime + jt, ktraj] - 1im * dyHet2[end - Ntime + jt, ktraj]) + 
-                    sqrt(etavalD) * (cD * dyDep[end - Ntime + jt, ktraj])
-        end
-            
         for jomega = 1:(Nomega+1)
         #in questo ciclo mi calcolo le likelihood della misura al tempo jt per ciascun valore di omega
             
-            #H = (omegay[jomega]/2.) * sy; # Hamiltonian of the qubit
-
-            M = M1 - 1im * SMatrix{2,2}(H[jomega]) * dt
             rhotmp = SMatrix{2,2}(view(rho,:,:,jomega))
-#            rhotmp = @views rho[:,:,jomega]
-            newRho = M * rhotmp * M'
-            if  jt<=3
-                newRho += dt * (cF * rhotmp * cF') 
-                newRho += dt * (cD * rhotmp * cD')
-                newRho += dt * (cPhi * rhotmp * cPhi');
-            else        
-                newRho += (1 - etavalF) * dt * (cF * rhotmp * cF') +  (1 - etavalD) * dt * (cD * rhotmp * cD') +  dt * (cPhi * rhotmp * cPhi');
-            end 
-            
-            lklhood[jomega] = real(tr(newRho));
+            newRho = similar(rhotmp)
+            for intt = 1 : internalsteps
+                offset = - Ntime * internalsteps + jt * internalsteps + intt - internalsteps
                 
-            rho[:,:,jomega] = newRho / lklhood[jomega];
+                if jt <= unconditional_timesteps
+                    M1 = M0;
+                else
+                    M1 = M0 + sqrt(etavalF/2) * cF * (dyHet1[end + offset, ktraj] - 1im * dyHet2[end + offset, ktraj]) + sqrt(etavalD) * (cD * dyDep[end + offset, ktraj])
+                end
+                    
+                M = M1 - 1im * SMatrix{2,2}(H[jomega]) * idt
+    #            rhotmp = @views rho[:,:,jomega]
+                newRho = M * rhotmp * M'
+                if  jt<=3
+                    newRho += idt * (cF * rhotmp * cF') 
+                    newRho += idt * (cD * rhotmp * cD')
+                    newRho += idt * (cPhi * rhotmp * cPhi');
+                else        
+                    newRho += (1 - etavalF) * idt * (cF * rhotmp * cF') +  (1 - etavalD) * idt * (cD * rhotmp * cD') +  idt * (cPhi * rhotmp * cPhi');
+                end
+
+                rhotmp = copy(newRho)
+            end
+
+            lklhood[jomega] = real(tr(newRho))
+                
+            rho[:,:,jomega] = newRho / lklhood[jomega]
 
             # Correction factor
-            lklhood[jomega] = lklhood[jomega] - (omegay[jomega] * dt / 2)^2
+            lklhood[jomega] = lklhood[jomega] - internalsteps * (omegay[jomega] * idt / 2)^2
                             
             if abs(omegay[jomega]-omegaTrue) < domega    
-                AvgZcond[ktraj,jt] = real(tr(rho[:,:,jomega]*sz));
+                AvgZcond[ktraj,jt] = real(tr(rho[:,:,jomega]*sz))
             end
 
             if jt == Ntime
-                pPlus = real(tr(rho[:,:,jomega]*PiPlus));
+                pPlus = real(tr(rho[:,:,jomega]*PiPlus))
                 if OutZ[ktraj] >= threshold
-                    lklhoodStrong[jomega] = pPlus;
+                    lklhoodStrong[jomega] = pPlus
                 else
-                    lklhoodStrong[jomega] = (1 - pPlus);
+                    lklhoodStrong[jomega] = (1 - pPlus)
                 end
             end
         end  # fine ciclo su omega
-        
+
         if jt == 1
             lklhoodTraj = lklhood  #likelihood singola traiettoria
             if ktraj == 1
