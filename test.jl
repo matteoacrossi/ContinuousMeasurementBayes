e1d81ff4c4dae4c7b89c84c3246ba58f3f799d5c
Matteo Rossi <teo.red90@gmail.com>
2019-03-13 09:22:48 +0200

    test.jl now works

--- test.jl
+++ test.jl
@@ -1,52 +1,34 @@
-using JLD2
+# Experimental parameters
+T1 = 15.0
+TD = 5.0
+Trabi = 2.
+Tphi = 17.9
 
-# Parameters
-Ntrajectories = 10
-Tfinal = 20. # Final time
-dt = 0.1 # duration of infinitesimal time
-Gamma1 = 1. / 15   # Gamma fluoresence
-GammaD = 1. / 0.3   # Gamma dephasing controllable
-GammaPhi = 1. / 17.9  # Gamma dephasing not controllable
-etavalF = 0.14 # efficiency fluoresence heterodyne
-etavalD = 0.34 # efficiency dephasing homodyne
-omegaTrue = 2 * pi / 5. # True value of omega
-omegaMin= 0. # minimum value of omega
-omegaMax= 3. # maximum value of omega
-Nomega = 200 # Resolution in omega for the Bayesian estimation
+NTrajectories = 100
 
-#@save "parameters.jld"
+# Parameter dictionary to be passed to the functions
+params = Dict( :Tfinal    => 20., # Final time
+               :dt        => 0.1, # duration of infinitesimal time
+               :Gamma1    => 1. / T1,   # Gamma fluoresence
+               :GammaD    => 1. / TD,  # Gamma dephasing controllable
+               :GammaPhi  => 1. / Tphi,  # Gamma dephasing not controllable
+               :etavalF   => 0.14, # efficiency fluoresence heterodyne
+               :etavalD   => 0.34, # efficiency dephasing homodyne
+               :omegaTrue => 2 * pi / Trabi, # True value of omega
+               :omegaMin  => 2., # minimum value of omega
+               :omegaMax  => 4., # maximum value of omega
+               :threshold => 0.375,
+               :Nomega    => 500); # Resolution in omega for the Bayesian estimation
+
 
 include("fluo_cont_meas_sim.jl")
-include("likelihood_all.jl")
+include("likelihood_strong.jl")
 
-@time (t, Ntime, dyHet1, dyHet2, dyDep, OutStrong, AvgZCondTrue) = parallel_fluo_continuous_measurement_het_simulation(Ntrajectories;
-        Tfinal = Tfinal, # Final time
-        dt = dt, # duration of infinitesimal time
-        Gamma1 = Gamma1 ,   # Gamma fluoresence
-        GammaD = GammaD,    # Gamma dephasing controllable
-        GammaPhi = GammaPhi,  # Gamma dephasing not controllable
-        etavalF=etavalF, #efficiency fluoresence heterodyne
-        etavalD=etavalD, #efficiency dephasing homodyne
-        omegay = omegaTrue) # omegaz
-
-@time (t, omegay, AvgZcond, probBayes, probBayesTraj, omegaEst, omegaMaxLik, sigmaBayes)= LikelihooodTrajNew(Ntrajectories, Ntime;
-    Tfinal = Tfinal, # Final time
-    Gamma1 = Gamma1 ,   # Gamma fluoresence
-    GammaD = GammaD,    # Gamma dephasing controllable
-    GammaPhi = GammaPhi,  # Gamma dephasing not controllable
-    etavalF = etavalF, #efficiency fluoresence heterodyne
-    etavalD = etavalD, #efficiency dephasing homodyne
-    Nomega = Nomega, # number of values of omega
-    omegaTrue = omegaTrue, #true value of omega
-    omegaMin = omegaMin,
-    omegaMax = omegaMax,
-    dyHet1 = dyHet1, #output current 1 heterodyne (simgax)
-    dyHet2 = dyHet2, #output current 2 heterodyne (simgay)
-    dyDep = dyDep,  #output current 3 homodyne (sigmaz)
-    OutS = OutStrong ); # output strong final measurement
-
+@time simData = parallel_fluo_continuous_measurement_het_simulation(NTrajectories; params...) 
+@time simRes = Likelihood_strong(simData.dyHet1, simData.dyHet2, simData.dyDep, simData.OutStrong, 200; 
+        internalsteps = 1, params...);
 #@save "all_data.jld" t Ntime dyHet1 dyHet2 dyDep OutStrong AvgZCondTrue omegay AvgZcond probBayes probBayesTraj omegaEst omegaMaxLik sigmaBayes
 #@save "data.jld" t Ntime omegay probBayes omegaEst omegaMaxLik sigmaBayes
 
 # Test if the average values of z coincide
-@assert isapprox(AvgZcond, AvgZCondTrue, rtol=dt^2,atol=dt^2)
+@assert isapprox(simData.AvgZcondTrue, simRes.AvgZcond, rtol=params[:dt]^2,atol=params[:dt]^2)
