38593bc36788dae046c447949e044f8befb553d2
Matteo Rossi <teo.red90@gmail.com>
2019-04-02 12:52:11 +0200

    Test uses datasets.jl

--- test.jl
+++ test.jl
@@ -1,29 +1,18 @@
-# Experimental parameters
-T1 = 15.0
-TD = 5.0
-Trabi = 2.
-Tphi = 17.9
 
 NTrajectories = 100
 
-# Parameter dictionary to be passed to the functions
-params = Dict( :Tfinal    => 20., # Final time
-               :dt        => 0.1, # duration of infinitesimal time
-               :Gamma1    => 1. / T1,   # Gamma fluoresence
-               :GammaD    => 1. / TD,  # Gamma dephasing controllable
-               :GammaPhi  => 1. / Tphi,  # Gamma dephasing not controllable
-               :etaF   => 0.14, # efficiency fluoresence heterodyne
-               :etaD   => 0.34, # efficiency dephasing homodyne
-               :omegaTrue => 2 * pi / Trabi, # True value of omega
-               :omegaMin  => 2., # minimum value of omega
-               :omegaMax  => 4., # maximum value of omega
-               :threshold => 0.375, # Threshold for final measurement (experimental data)
-               :Nomega    => 500, # Resolution in omega for the Bayesian estimation
-               :unconditional_timesteps => 3);
-
-
+include("datasets.jl")
 include("fluo_cont_meas_sim.jl")
 include("likelihood_strong.jl")
+
+estparams = Dict(
+        :omegaMin  => 0., # minimum value of omega
+        :omegaMax  => 5., # maximum value of omega
+        :Nomega => 200)
+
+params = merge(experimental_params[1], estparams)
+
+@show params
 
 @time simData = parallel_fluo_continuous_measurement_het_simulation(NTrajectories; params...) 
 @time simRes = likelihood_strong(simData; 
@@ -32,4 +21,5 @@ include("likelihood_strong.jl")
 # Test if the average values of z coincide
 @assert isapprox(simData.AvgZcondTrue, simRes.AvgZcond, rtol=params[:dt]^2,atol=params[:dt]^2)
 
-println("Test passed!")
+
+println("Tests passed!")
