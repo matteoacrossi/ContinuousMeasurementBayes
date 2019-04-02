6986c3ffa80afb3d94336386de69fda436ef23fb
Matteo Rossi <teo.red90@gmail.com>
2019-04-02 17:07:48 +0200

    All the trajectories!

--- run_estimation.jl
+++ run_estimation.jl
@@ -2,4 +2,4 @@ include("do_estimation.jl")
 
 @show ARGS
 expid = Base.parse(Int64, ARGS[1])
-do_estimation([10 100 1000], experimental_params[expid])
+do_estimation([10 100 1000 10000 100000 500000], experimental_params[expid])
