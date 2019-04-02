8ad58605ed24f04b1ba5a32b472cb2dab998bece
Matteo Rossi <teo.red90@gmail.com>
2019-04-02 12:52:19 +0200

    Polishing

--- datasets.jl
+++ datasets.jl
@@ -1,8 +1,3 @@
 using JSON
 symb_dict(d::Dict) = Dict(Symbol(k) => v for (k, v) in d)
-expparams = symb_dict.(JSON.parse(open("params.json")))
-
-estparams = Dict(
-:omegaMin  => 2., # minimum value of omega
-:omegaMax  => 4., # maximum value of omega
-:Nomega => 200)
+experimental_params = symb_dict.(JSON.parse(open("params.json")))
