7d9fcfe1daaca0e1706a1060113c2e278fc29f5d
Matteo Rossi <teo.red90@gmail.com>
2019-04-03 17:46:40 +0200

    More trajectories points

--- run_estimation.jl
+++ run_estimation.jl
@@ -2,4 +2,4 @@ include("do_estimation.jl")
 
 @show ARGS
 expid = Base.parse(Int64, ARGS[1])
-do_estimation([50 100 500 750 1000 2500 5000 10000], experimental_params[expid])
+do_estimation(vcat([50, 100], collect(500:500:10000)), experimental_params[expid])
